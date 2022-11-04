b4dac81060b97617341b756013957c55b9015481
Raine <raineen20@gcc.edu>
2022-11-04 15:10:23 -0400

    Add score to matches

--- world_cup/worldcup_dataloader.py
+++ world_cup/worldcup_dataloader.py
@@ -1,7 +1,7 @@
 import os
 import json
 from datetime import datetime
-from marshmallow import Schema, fields, validate, post_loadc
+from marshmallow import Schema, fields, validate, post_load
 
 # define a simple Python class representing a Stadium
 class Stadium(object):
@@ -73,30 +73,34 @@ class MatchSchema(Schema):
     stadium = fields.Nested(StadiumSchema())
     home_team = fields.Nested(TeamSchema())
     away_team = fields.Nested(TeamSchema())
+    score_home = fields.Int(load_default = None)
+    score_away = fields.Int(load_default = None)
 
     @post_load
     def make_match(self, data, many: bool, partial: bool):
         if many:
             return [Match(d['id'], d['group'], d['date'], 
-                        d['stadium'], d['home_team'], d['away_team']) \
+                        d['stadium'], d['home_team'], d['away_team'], d['score_home'], d['score_away']) \
                             for d in data]
         else:
             d = data
             return Match(d['id'], d['group'], d['date'], 
-                        d['stadium'], d['home_team'], d['away_team'])
+                        d['stadium'], d['home_team'], d['away_team'], d['score_home'], d['score_away'])
     
 
 
 # define a simple Python class representing a Match
 class Match(object):
     def __init__(self, id: int, group: str, date: datetime, stadium: Stadium,
-                    home_team: Team, away_team: Team):
+                    home_team: Team, away_team: Team, score_home: int, score_away: int):
         self.id = id
         self.group = group
         self.date = date
         self.stadium = stadium
         self.home_team = home_team
         self.away_team = away_team
+        self.score_home = score_home
+        self.score_away = score_away
     
     def __str__(self):
         return f"Match({self.home_team.name} vs {self.away_team.name})"
