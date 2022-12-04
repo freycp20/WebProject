19bc3f2fc50cade8861b3482ab57b8313f93cc0c
Caleb Frey <freycp20@gcc.edu>
2022-12-04 18:27:23 -0500

    fixed button on create bracket and added href

--- world_cup/static/bracket.js
+++ world_cup/static/bracket.js
@@ -59,6 +59,7 @@ function checkIfComplete() {
     }
     doRequest(data).then(resp => {
         console.log(resp);
+        location.href = "http://127.0.0.1:5000/view-bracket/"
     });
     // let num_teams = 8
 
